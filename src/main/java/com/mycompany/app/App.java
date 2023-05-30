fe7325a2cdb39518215f95c6d92dedfbdcfda4f6
clabca <45792137+clabca@users.noreply.github.com>
2023-05-30 18:37:47 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,81 +11,34 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
+
+        // Malas prácticas agregadas:
+
+        // Uso de variables no utilizadas
+        int unusedVariable = 5;
+
+        // Bloque de código sin indentación
+        for (int i = 0; i < 10; i++) {
+        System.out.println(i);
+        }
+
+        // Comentarios inapropiados
+        int x = 0; // Este es un contador
+
+        // Nombres de variables poco descriptivos
+        int c = 15;
+
+        // Uso incorrecto de cadenas
+        String s = new String("Example");
+        if (s.equals("Example")) {
+            // ...
+        }
+
+        // Clase con demasiadas responsabilidades
+        // Métodos largos y complejos
     }
 
     public String getMessage() {
         return MESSAGE;
     }
 }
-
-
-public class BadCodeExample {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 0;
-        
-        // División entre cero
-        int result = a / b;
-        
-        // Uso de variables no utilizadas
-        int unusedVariable = 5;
-        
-        // Código duplicado
-        System.out.println("Hello, world!");
-        System.out.println("Hello, world!");
-        
-        // Uso de comentarios inapropiados
-        int x = 0; // x es el contador
-        
-        // Nombres de variables poco descriptivos
-        int c = 15;
-        
-        // Uso de variables globales
-        System.out.println(globalVariable);
-        
-        // Uso de valores mágicos
-        if (a == 5) {
-            // ...
-        }
-        
-        // Bloque de try-catch vacío
-        try {
-            // ...
-        } catch (Exception e) {
-            
-        }
-        
-        // Uso incorrecto de cadenas
-        String s = "Example";
-        if (s == "Example") {
-            // ...
-        }
-    }
-    
-    // Métodos con demasiados parámetros
-    public void calculate(int a, int b, int c, int d, int e, int f) {
-        // ...
-    }
-    
-    // Métodos con demasiadas líneas de código
-    public void longMethod() {
-        // Línea 1
-        // Línea 2
-        // ...
-        // Línea 100
-    }
-    
-    // Métodos sin documentación
-    public void undocumentedMethod() {
-        // ...
-    }
-    
-    // Clase con demasiadas responsabilidades
-    // Métodos largos y complejos
-    
-    // Uso incorrecto de excepciones
-    public void doSomething() throws Exception {
-        // ...
-    }
-}
-
