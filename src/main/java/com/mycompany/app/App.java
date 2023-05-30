f15fc84fac7a6f415d68c3faa37da26dfe43864e
clabca <45792137+clabca@users.noreply.github.com>
2023-05-30 18:46:27 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,3 +1,4 @@
+
 package com.mycompany.app;
 
 /**
@@ -11,31 +12,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
-
-        // Malas prácticas agregadas:
-
-        // Uso de variables no utilizadas
-        int unusedVariable = 5;
-
-        // Bloque de código sin indentación
-        for (int i = 0; i < 10; i++) {
-        System.out.println(i);
-        }
-
-        // Comentarios inapropiados
-        int x = 0; // Este es un contador
-
-        // Nombres de variables poco descriptivos
-        int c = 15;
-
-        // Uso incorrecto de cadenas
-        String s = new String("Example");
-        if (s.equals("Example")) {
-            // ...
-        }
-
-        // Clase con demasiadas responsabilidades
-        // Métodos largos y complejos
     }
 
     public String getMessage() {
